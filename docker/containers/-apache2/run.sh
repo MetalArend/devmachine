741e98391dc1fbfdad06e24c5323bd3f5e1c8eda
MetalArend <metalarend@gmail.com>
2015-02-24 14:49:21 +0100

    make sure the log dir is present

--- docker/containers/-apache2/run.sh
+++ docker/containers/-apache2/run.sh
@@ -6,5 +6,5 @@
 
 CONTAINER_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 CONTAINER_NAME="$(basename "${CONTAINER_DIR}")"
-
+mkdir -p "${CONTAINER_DIR}/data/log"
 sudo docker run --name "${CONTAINER_NAME}" -d -t --volume "${CONTAINER_DIR}/data/log":"/log" --volumes-from "projects" "devmachine:${CONTAINER_NAME}-image" "-DFOREGROUND"
