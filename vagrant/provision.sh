32e58d278cb2f257448580d3b92b42f193af6188
MetalArend <metalarend@gmail.com>
2015-02-25 13:45:42 +0100

    remove \r

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -1,37 +1,1 @@
-#!/usr/bin/env bash
-
-# ====== ====== ====== ====== ====== ======
-# Vagrant - http://docs.vagrantup.com/v2/
-# ====== ====== ====== ====== ====== ======
-
-# Check current directory
-CWD="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-CONFIG_DIR="${CWD}/../config"
-SHELL_SCRIPTS_DIR="${CWD}/../shell/scripts"
-
-# Set timezone
-# TODO cp /usr/share/zoneinfo/Europe/Brussels /etc/localtime
-
-# fix command crashed
-# TODO http://ivaniliev.com/sorry-command-not-found-has-crashed/
-
-# Variables
-source "${CONFIG_DIR}/config.sh"
-
-# Run docker
-bash "${SHELL_SCRIPTS_DIR}/devmachine/install-composer.sh" -d "${COMPOSER_DIR}"
-bash "${SHELL_SCRIPTS_DIR}/docker/install-docker.sh"
-bash "${SHELL_SCRIPTS_DIR}/docker/run-docker-containers.sh" "${DOCKER_CONTAINER_FILEPATHS}"
-
-# Run application provision
-if test -f "/app/devmachine/provision.sh"; then
-    echo " "
-    (cd "/app"; bash "/app/devmachine/provision.sh")
-fi
-
-# Print branding, environment and containers
-bash "${SHELL_SCRIPTS_DIR}/branding/print-branding.sh"
-bash "${SHELL_SCRIPTS_DIR}/os/report-os.sh"
-bash "${SHELL_SCRIPTS_DIR}/devmachine/report-composer.sh"
-bash "${SHELL_SCRIPTS_DIR}/docker/report-docker.sh"
-bash "${SHELL_SCRIPTS_DIR}/docker/report-docker-containers.sh" "${DOCKER_CONTAINER_FILEPATHS}"
+#!/usr/bin/env bash# ====== ====== ====== ====== ====== ======# Vagrant - http://docs.vagrantup.com/v2/# ====== ====== ====== ====== ====== ======# Check current directoryCWD="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"CONFIG_DIR="${CWD}/../config"SHELL_SCRIPTS_DIR="${CWD}/../shell/scripts"# Set timezone# TODO cp /usr/share/zoneinfo/Europe/Brussels /etc/localtime# fix command crashed# TODO http://ivaniliev.com/sorry-command-not-found-has-crashed/# Variablessource "${CONFIG_DIR}/config.sh"# Run dockerbash "${SHELL_SCRIPTS_DIR}/devmachine/install-composer.sh" -d "${COMPOSER_DIR}"bash "${SHELL_SCRIPTS_DIR}/docker/install-docker.sh"bash "${SHELL_SCRIPTS_DIR}/docker/run-docker-containers.sh" "${DOCKER_CONTAINER_FILEPATHS}"# Run application provisionif test -f "/app/devmachine/provision.sh"; then    echo " "    (cd "/app"; bash "/app/devmachine/provision.sh")fi# Print branding, environment and containersbash "${SHELL_SCRIPTS_DIR}/branding/print-branding.sh"bash "${SHELL_SCRIPTS_DIR}/os/report-os.sh"bash "${SHELL_SCRIPTS_DIR}/devmachine/report-composer.sh"bash "${SHELL_SCRIPTS_DIR}/docker/report-docker.sh"bash "${SHELL_SCRIPTS_DIR}/docker/report-docker-containers.sh" "${DOCKER_CONTAINER_FILEPATHS}"
