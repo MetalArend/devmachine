fe840fe6b391d539cce06b88f4e08527f843e701
MetalArend <metalarend@gmail.com>
2015-02-26 16:49:51 +0100

    convert to lf line endings on windows

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -7,16 +7,6 @@
 # Check current directory
 CWD="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# Convert to linux line-endings for cygwin
-case "$(uname)" in
-    CYGWIN*)
-        echo "Convert to LF files"
-        find "${CWD}/../" -name "*.sh" -type f -exec sed -i.bak 's/\r$//g' {} \;
-        ;;
-    *)
-        ;;
-esac
-
 # Load configuration
 CONFIG_DIR="${CWD}/../config"
 SHELL_SCRIPTS_DIR="${CWD}/../shell/scripts"
