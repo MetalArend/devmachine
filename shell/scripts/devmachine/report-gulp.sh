ee00027d7e989cf02c0398d541a1f9188fee1120
MetalArend <metalarend@gmail.com>
2015-03-04 10:02:42 +0100

    remove newline from gulp version

--- shell/scripts/devmachine/report-gulp.sh
+++ shell/scripts/devmachine/report-gulp.sh
@@ -30,7 +30,7 @@ fi
 
 # Report
 if which gulp &> /dev/null; then
-    GULP_VERSION=$(gulp --version)
+    GULP_VERSION=$(gulp --version | sed 's/^\s*\[[0-9\:]*\]\s*//g' | sed ':a;N;$!ba;s/\n/, /g' | sed -r 's/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g')
     echo -e "\e[92m- gulp ${GULP_VERSION}\e[0m"
 else
     echo -e "\e[91m- gulp not found\e[0m"
