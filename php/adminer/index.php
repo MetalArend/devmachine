5c94fa7a466fc245379653a6ba7c0afffd0e7874
MetalArend <metalarend@gmail.com>
2015-03-04 10:50:55 +0100

    update adminer ip

--- php/adminer/index.php
+++ php/adminer/index.php
@@ -2,7 +2,11 @@
 error_reporting(E_ALL);
 ini_set('display_errors', true);
 
+$ip = reset(array_filter(array_merge(gethostbynamel(gethostname()), array(isset($_SERVER['HTTP_HOST']) ? strtok($_SERVER['HTTP_HOST'], ':') : '')), function ($ip) {
+    return 0 === strpos($ip, "192.168.");
+}));
+
 $files = array_diff(scandir('.', SCANDIR_SORT_DESCENDING), array('.', '..', 'index.php'));
 
-header('Location: ' . array_shift($files) . '?server=192.168.10.10&username=docker&db=mysql');
+header('Location: ' . array_shift($files) . '?server=' . $ip . '&username=docker&db=mysql');
 exit;
