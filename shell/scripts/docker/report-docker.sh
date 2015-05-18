cc7954863bdc3d208d0a172806bc59e003166cfa
MetalArend <metalarend@gmail.com>
2015-05-18 10:29:57 +0200

    report docker on one line

--- shell/scripts/docker/report-docker.sh
+++ shell/scripts/docker/report-docker.sh
@@ -6,8 +6,7 @@
 
 if which docker &> /dev/null; then
     DOCKER_VERSION=$(docker --version | sed 's/^[^0-9]* //g')
-    echo -e "\e[92m- docker ${DOCKER_VERSION}\e[0m"
-    echo -e "\e[92m  $(docker info 2>/dev/null | sed -n -e '/Containers:.*/,/Images:.*/p' | sed ':a;N;s/\n/, /g')\e[0m"
+    echo -e "\e[92m- docker ${DOCKER_VERSION} ($(docker info 2>/dev/null | sed -n -e '/Containers:.*/,/Images:.*/p' | sed ':a;N;s/\n/, /g'))\e[0m"
 else
     echo -e "\e[91m- docker not found\e[0m"
 fi
