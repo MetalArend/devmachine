c62ab7a03b387422044f876f30adf72b87cbb4d4
MetalArend <metalarend@gmail.com>
2015-06-09 13:55:43 +0200

    combine docker-compose with docker

--- shell/scripts/docker/install-docker.sh
+++ shell/scripts/docker/install-docker.sh
@@ -4,20 +4,38 @@
 # Docker - own implementation, as http://docs.vagrantup.com/v2/provisioning/docker.html is too limited
 # ====== ====== ====== ====== ====== ======
 
-# Install
+# Install docker
 if ! which docker &> /dev/null; then
     # Run install script from official docker server
     curl -sL https://get.docker.io/ | sh
 
-    # Add the docker group
-    sudo groupadd docker
+    # Run install of older docker version
+#    sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+    #echo "Acquire::HTTP::Proxy::get.docker.com \"DIRECT\";" | sudo tee /etc/apt/apt.conf.d/01proxy
+#    echo deb https://get.docker.com/ubuntu docker main | sudo tee /etc/apt/sources.list.d/docker.list
+#    sudo apt-get update; sudo apt-get install -y -q lxc-docker-1.6.0
 
-    # Add the root and vagrant users to the docker group # TIP: don't use usermod!
-    sudo gpasswd docker -a "${USER}"
-    sudo gpasswd docker -a "vagrant"
+    if which docker &> /dev/null; then
+        # Add the docker group
+        sudo groupadd docker
+        sudo usermod -aG docker vagrant
+
+        # Add the root and vagrant users to the docker group # TIP: don't use usermod!
+        sudo gpasswd -a "${USER}" docker
+        sudo gpasswd -a "vagrant" docker
+    fi
+fi
+
+# Install docker-compose
+if ! which docker-compose &> /dev/null; then
+    # Run install script from official docker compose github
+    curl -L https://github.com/docker/compose/releases/download/1.2.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+    chmod +x /usr/local/bin/docker-compose
 fi
 
 # Restart the daemon
 if which service &> /dev/null; then
-    sudo service docker restart
+    if which docker &> /dev/null; then
+        sudo service docker restart
+    fi
 fi
--- shell/scripts/docker/report-docker.sh
+++ shell/scripts/docker/report-docker.sh
@@ -10,3 +10,10 @@ if which docker &> /dev/null; then
 else
     echo -e "\e[91m- docker not found\e[0m"
 fi
+
+if which docker-compose &> /dev/null; then
+    DOCKER_COMPOSE_VERSION=$(docker-compose --version | sed 's/^[^0-9]* //g')
+    echo -e "\e[92m- docker-compose ${DOCKER_COMPOSE_VERSION}\e[0m"
+else
+    echo -e "\e[91m- docker-compose not found\e[0m"
+fi
