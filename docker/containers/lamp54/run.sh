1a7b89ba9c6ce56fcad6082802e17ca424bae1e8
MetalArend <metalarend@gmail.com>
2015-06-09 14:42:21 +0200

    update apache2 config as separate file

--- docker/containers/lamp54/run.sh
+++ docker/containers/lamp54/run.sh
@@ -10,7 +10,6 @@ CONTAINER_NAME="$(basename "${CONTAINER_DIR}")"
 DB_CONTAINER_NAME="mysql56"
 PHP_FPM_CONTAINER_NAME="php54-fpm"
 
-# TODO multiple ports 80 don't show up in the reporting
 sudo docker run --name "${CONTAINER_NAME}" -p 8054:80 -p 80:80 -d -t --volumes-from "data" \
 --link "${PHP_FPM_CONTAINER_NAME}":"php" --link "${DB_CONTAINER_NAME}":"db" \
 "devmachine:${CONTAINER_NAME}-image" -DFOREGROUND
