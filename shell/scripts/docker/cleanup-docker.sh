d4f77a82b1ef0f9ef8ed87fec60976402b702e8c
MetalArend <metalarend@gmail.com>
2015-06-12 11:49:54 +0200

    remove all containers and images one by one

--- shell/scripts/docker/cleanup-docker.sh
+++ shell/scripts/docker/cleanup-docker.sh
@@ -8,21 +8,27 @@
 CONTAINERS_IDS_DEAD="$(sudo docker ps --all --no-trunc | grep " Dead " | awk '{print $1}')"
 if test -n "${CONTAINERS_IDS_DEAD}"; then
     for CONTAINER_ID_DEAD in ${CONTAINERS_IDS_DEAD}; do
-        echo -e "\e[93mRemove dead container '${CONTAINER_ID_DEAD}'\e[0m"
+        echo -e "\e[93mRemove container \"${CONTAINER_ID_DEAD}\" (dead container)\e[0m"
         sudo docker rm -f "${CONTAINER_ID_DEAD}"
     done
 fi
 
 # Remove exited containers not with exit 0
-if test -n "$(sudo docker ps --all --no-trunc | grep "Exit " | grep -v "Exit 0")"; then
-    echo -e "\e[93mRemove exited containers not with exit 0\e[0m"
-    sudo docker rm -f $(sudo docker ps --all --no-trunc | grep "Exit " | grep -v "Exit 0" | awk '{print $1}')
+CONTAINERS_IDS_EXITED="$(sudo docker ps --all --no-trunc | grep "Exit " | grep -v "Exit 0" | awk '{print $1}')"
+if test -n "${CONTAINERS_IDS_EXITED}"; then
+    for CONTAINER_ID_EXITED in ${CONTAINERS_IDS_EXITED}; do
+        echo -e "\e[93mRemove container \"${CONTAINER_ID_EXITED}\" (exited, not with exit code 0)\e[0m"
+        sudo docker rm -f "${CONTAINER_ID_EXITED}"
+    done
 fi
 
 # Remove exited containers without a tag
-if test -n "$(sudo docker ps --all --no-trunc | grep "Exit 0" | awk '{print $1,$2}' | grep -v ":")"; then
-    echo -e "\e[93mRemove exited containers without a tag\e[0m"
-    sudo docker rm -f $(sudo docker ps --all --no-trunc | grep "Exit 0" | awk '{print $1,$2}' | grep -v ":" | awk '{print $1}')
+CONTAINERS_IDS_UNTAGGED="$(sudo docker ps --all --no-trunc | grep "Exit 0" | awk '{print $1,$2}' | grep -v ":" | awk '{print $1}')"
+if test -n "${CONTAINERS_IDS_UNTAGGED}"; then
+    for CONTAINER_ID_UNTAGGED in ${CONTAINERS_IDS_UNTAGGED}; do
+        echo -e "\e[93mRemove container \"${CONTAINER_ID_UNTAGGED}\" (exited, without a tag)\e[0m"
+        sudo docker rm -f "${CONTAINER_ID_UNTAGGED}"
+    done
 fi
 
 # Remove unused images
@@ -35,7 +41,6 @@ for CONTAINER_ID in ${CONTAINER_IDS_ALL}; do
 done
 IMAGE_IDS_ALL_UNUSED=$(comm -23 <(echo "${IMAGE_IDS_ALL}" | sort -u) <(echo "${IMAGE_IDS_ALL_USED}" | sort -u))
 if test -n "${IMAGE_IDS_ALL_UNUSED}"; then
-    echo -e "\e[93mRemove images not used by any containers\e[0m"
     HISTORY_AND_IMAGE_IDS=""
     for IMAGE_ID_UNUSED in ${IMAGE_IDS_ALL_UNUSED}; do
         HISTORY_AND_IMAGE_IDS=$(echo -e "${HISTORY_AND_IMAGE_IDS}\n$(sudo docker history --quiet --no-trunc "${IMAGE_ID_UNUSED}" | wc -l) ${IMAGE_ID_UNUSED}")
@@ -43,6 +48,7 @@ if test -n "${IMAGE_IDS_ALL_UNUSED}"; then
     IMAGE_IDS_ALL_UNUSED="$(echo "${HISTORY_AND_IMAGE_IDS}" | sort --unique --reverse | awk '{print $2}')"
     for IMAGE_ID_UNUSED in ${IMAGE_IDS_ALL_UNUSED}; do
         if test -n "$(sudo docker images --all --quiet --no-trunc | grep "${IMAGE_ID_UNUSED}")"; then
+            echo -e "\e[93mRemove image \"${IMAGE_ID_UNUSED}\" (not used by any container)\e[0m"
             sudo docker rmi --force "${IMAGE_ID_UNUSED}"
         fi
     done
@@ -54,9 +60,9 @@ if sudo test -d "/var/lib/docker/containers"; then
     CONTAINERS_USED=$(sudo docker ps --all --quiet --no-trunc | grep -v " DEAD " | sort -u)
     CONTAINERS_DIRECTORIES_TO_DELETE=$(comm -23 <(echo -e "${CONTAINERS_DIRECTORIES}" | sort -u) <(echo -e "${CONTAINERS_USED}" | sort -u))
     if test -n "${CONTAINERS_DIRECTORIES_TO_DELETE}"; then
-        echo -e "\e[93mRemove unused container directories\e[0m"
-        for CONTAINERS_DIRECTORY_TO_DELETE in ${CONTAINERS_DIRECTORIES_TO_DELETE}; do
-            sudo rm -rf "/var/lib/docker/containers/${CONTAINERS_DIRECTORY_TO_DELETE}"
+        for CONTAINER_DIRECTORY_TO_DELETE in ${CONTAINERS_DIRECTORIES_TO_DELETE}; do
+            echo -e "\e[93mRemove unused container directory \"/var/lib/docker/containers/${CONTAINER_DIRECTORY_TO_DELETE}\"\e[0m"
+            sudo rm -rf "/var/lib/docker/containers/${CONTAINER_DIRECTORY_TO_DELETE}"
         done
     fi
 fi
@@ -67,9 +73,9 @@ if sudo test -d "/var/lib/docker/volumes"; then
     VOLUMES_USED=$(sudo docker inspect --format="$(echo '{{range $p, $conf := .Volumes}}{{if $conf}}{{$conf}}{{end}}\n{{end}}' | sed 's/\\n/\n/g')" $(sudo docker ps --all --quiet) | sort -u | grep -v "^$")
     VOLUMES_DIRECTORIES_TO_DELETE=$(comm -23 <(echo -e "${VOLUMES_DIRECTORIES}" | sort -u) <(echo -e "${VOLUMES_USED}" | sort -u))
     if test -n "${VOLUMES_DIRECTORIES_TO_DELETE}"; then
-        echo -e "\e[93mRemove unused volumes directories\e[0m"
-        for VOLUMES_DIRECTORY_TO_DELETE in ${VOLUMES_DIRECTORIES_TO_DELETE}; do
-            sudo rm -rf "${VOLUMES_DIRECTORY_TO_DELETE}"
+        for VOLUME_DIRECTORY_TO_DELETE in ${VOLUMES_DIRECTORIES_TO_DELETE}; do
+            echo -e "\e[93mRemove unused volume directory \"${VOLUME_DIRECTORY_TO_DELETE}\"\e[0m"
+            sudo rm -rf "${VOLUME_DIRECTORY_TO_DELETE}"
         done
     fi
 fi
@@ -80,8 +86,8 @@ if sudo test -d "/var/lib/docker/vfs/dir"; then
     VFS_USED=$(sudo docker inspect --format="$(echo '{{range $p, $conf := .Volumes}}{{if $conf}}{{$conf}}{{end}}\n{{end}}' | sed 's/\\n/\n/g')" $(sudo docker ps --all --quiet) | sort -u | grep -v "^$")
     VFS_DIRECTORIES_TO_DELETE=$(comm -23 <(echo -e "${VFS_DIRECTORIES}" | sort -u) <(echo -e "${VFS_USED}" | sort -u))
     if test -n "${VFS_DIRECTORIES_TO_DELETE}"; then
-        echo -e "\e[93mRemove unused vfs directories\e[0m"
         for VFS_DIRECTORY_TO_DELETE in ${VFS_DIRECTORIES_TO_DELETE}; do
+            echo -e "\e[93mRemove unused vfs directory \"${VFS_DIRECTORY_TO_DELETE}\"\e[0m"
             sudo rm -rf "${VFS_DIRECTORY_TO_DELETE}"
         done
     fi
