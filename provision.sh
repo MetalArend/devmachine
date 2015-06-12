53c823abfb57bdc5d9af43775c76b99cec8b6f2d
MetalArend <metalarend@gmail.com>
2015-06-12 11:49:29 +0200

    add space for empty rule (vagrant is not showing empty strings)

--- provision.sh
+++ provision.sh
@@ -46,5 +46,5 @@ bash "${SHELL_SCRIPTS_DIR}/docker/report-docker-containers.sh" "${DOCKER_CONTAIN
 echo " "
 
 # Run application provision
-find "${PHP_PROJECTS_DIR}" -iname "provision.sh" -exec echo -e "\e[93mRunning provisioning file \"{}\"\e[0m" \; -exec bash "{}" \; -exec echo "" \;
+find "${PHP_PROJECTS_DIR}" -iname "provision.sh" -exec echo -e "\e[93mRunning provisioning file \"{}\"\e[0m" \; -exec bash "{}" \; -exec echo " " \;
 echo " "
