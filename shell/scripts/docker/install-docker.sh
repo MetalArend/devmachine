bf6694d921872eee5f8ab90ca98f9fdfbcdebda5
MetalArend <metalarend@gmail.com>
2015-06-29 09:58:46 +0200

    install specific docker version

--- shell/scripts/docker/install-docker.sh
+++ shell/scripts/docker/install-docker.sh
@@ -7,13 +7,13 @@
 # Install docker
 if ! which docker &> /dev/null; then
     # Run install script from official docker server
-    curl -sSL https://get.docker.io/ | sh
+    #curl -sSL https://get.docker.io/ | sh
 
     # Run install of older docker version
-#    sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+    sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
     #echo "Acquire::HTTP::Proxy::get.docker.com \"DIRECT\";" | sudo tee /etc/apt/apt.conf.d/01proxy
-#    echo deb https://get.docker.com/ubuntu docker main | sudo tee /etc/apt/sources.list.d/docker.list
-#    sudo apt-get update; sudo apt-get install -y -q lxc-docker-1.6.0
+    echo deb https://get.docker.com/ubuntu docker main | sudo tee /etc/apt/sources.list.d/docker.list
+    sudo apt-get update; sudo apt-get install -y -q lxc-docker-1.6.0
 
     if which docker &> /dev/null; then
         # Add the docker group
