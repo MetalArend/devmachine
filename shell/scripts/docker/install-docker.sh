da8aedd7fc2eb13fe7f5d0ca47f20dd2a8432e78
MetalArend <metalarend@gmail.com>
2015-06-29 09:59:16 +0200

    install newer docker-compose version

--- shell/scripts/docker/install-docker.sh
+++ shell/scripts/docker/install-docker.sh
@@ -32,9 +32,8 @@ fi
 # Install docker-compose
 if ! which docker-compose &> /dev/null; then
     # Run install script from official docker compose github
-    curl -L https://github.com/docker/compose/releases/download/1.2.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+    curl -sL https://github.com/docker/compose/releases/download/1.3.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
-fi
 
 # Restart the daemon
 if which service &> /dev/null; then
