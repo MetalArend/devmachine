2d6aaee026405d19b43aa483c1e455177c008c7f
MetalArend <metalarend@gmail.com>
2015-06-29 10:02:54 +0200

    newer version of docker-compose has extra lines

--- shell/scripts/docker/report-docker.sh
+++ shell/scripts/docker/report-docker.sh
@@ -12,8 +12,8 @@ else
 fi
 
 if which docker-compose &> /dev/null; then
-    DOCKER_COMPOSE_VERSION=$(docker-compose --version | sed 's/^[^0-9]* //g')
-    echo -e "\e[92m- docker-compose ${DOCKER_COMPOSE_VERSION}\e[0m"
+    DOCKER_COMPOSE_VERSION=$(docker-compose --version | sed 's/version: //g' | sed ':a;N;$!ba;s/\n/, /g')
+    echo -e "\e[92m- ${DOCKER_COMPOSE_VERSION}\e[0m"
 else
     echo -e "\e[91m- docker-compose not found\e[0m"
 fi
