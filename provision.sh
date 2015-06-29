42f24cdc9ea8bd4ada326181a8c6637870a24082
MetalArend <metalarend@gmail.com>
2015-06-29 10:15:30 +0200

    remove obvious subdirectories

--- provision.sh
+++ provision.sh
@@ -8,20 +8,20 @@ CWD="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "${CWD}"
 
 # Configure system
-bash "${CWD}/shell/scripts/devmachine/configure-system.sh" -t "Europe/Brussels"
+bash "${CWD}/shell/devmachine/configure-system.sh" -t "Europe/Brussels"
 
 # Install
-bash "${CWD}/shell/scripts/docker/install-docker.sh"
+bash "${CWD}/shell/docker/install-docker.sh"
 
 # Print branding & report environment
-bash "${CWD}/shell/scripts/branding/print-branding.sh"
-bash "${CWD}/shell/scripts/os/report-os.sh"
+bash "${CWD}/shell/branding/print-branding.sh"
+bash "${CWD}/shell/os/report-os.sh"
 
 # Report docker
-bash "${CWD}/shell/scripts/docker/report-docker.sh"
+bash "${CWD}/shell/docker/report-docker.sh"
 
 # Cleanup
-bash "${CWD}/shell/scripts/docker/cleanup-docker.sh"
+bash "${CWD}/shell/docker/cleanup-docker.sh"
 
 # Run application provision
 find "/env/workspace" -iname "docker-provision.sh" -exec echo -e "\e[93mRun shell script \"{}\"\e[0m" \; -exec bash "{}" \; -exec echo " " \;
