bd5daff623a94df6fbfb3c5a613f50e2980e1413
MetalArend <metalarend@gmail.com>
2015-07-29 09:53:24 +0200

    add project provisioning to vagrant

--- provision.sh
+++ provision.sh
@@ -19,6 +19,3 @@ bash "${CWD}/shell/cleanup-docker.sh"
 # Report
 bash "${CWD}/shell/report-os.sh"
 bash "${CWD}/shell/report-docker.sh"
-
-# Run application provision
-find "/env/workspace" -iname "docker-provision.sh" -exec echo -e "\e[93mRun shell script \"{}\"\e[0m" \; -exec bash "{}" \; -exec echo " " \;
