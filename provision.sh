0328b18f1d432e93ce0ed7d2a52e02dc4dd7f8a7
MetalArend <metalarend@gmail.com>
2015-07-29 09:51:42 +0200

    simplify shell scripting

--- provision.sh
+++ provision.sh
@@ -8,19 +8,17 @@ CWD="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "${CWD}"
 
 # Configure system
-bash "${CWD}/shell/devmachine/configure-system.sh" -t "Europe/Brussels"
+bash "${CWD}/shell/configure-system.sh" -t "Europe/Brussels"
 
 # Install
-bash "${CWD}/shell/docker/install-docker.sh"
-
-# Print branding & report environment
-bash "${CWD}/shell/os/report-os.sh"
-
-# Report docker
-bash "${CWD}/shell/docker/report-docker.sh"
+bash "${CWD}/shell/install-docker.sh"
 
 # Cleanup
-bash "${CWD}/shell/docker/cleanup-docker.sh"
+bash "${CWD}/shell/cleanup-docker.sh"
+
+# Report
+bash "${CWD}/shell/report-os.sh"
+bash "${CWD}/shell/report-docker.sh"
 
 # Run application provision
 find "/env/workspace" -iname "docker-provision.sh" -exec echo -e "\e[93mRun shell script \"{}\"\e[0m" \; -exec bash "{}" \; -exec echo " " \;
