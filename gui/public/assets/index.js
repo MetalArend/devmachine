e81381c2c4392b4980225963fa73b98257666b5b
MetalArend <metalarend@gmail.com>
2016-02-08 08:39:19 +0100

    move the backend for the devmachine to own directory

---
+++ gui/public/assets/index.js
@@ -0,0 +1,37 @@
+function loadFrame(iframe) {
+    var $iframe = $(iframe);
+    if (1 === $iframe.length) {
+        // Avoid flicker when reloading, by cloning into a new iframe instead of reloading the src
+        var $iframeReloaded = $iframe.clone(true, true);
+        $iframeReloaded.one('load', (function ($iframe, $iframeReloaded) {
+            return function () {
+                $iframeReloaded.show().attr('height', '0').attr('height', ($iframeReloaded.get(0).contentWindow.document.body.scrollHeight * 1.001) + 'px');
+                $iframe.remove();
+            };
+        }($iframe, $iframeReloaded)));
+        $iframeReloaded.hide().insertBefore($iframe);
+    }
+}
+$(document).ready(function () {
+    $('iframe').hide();
+    var $tabs = $('a[data-toggle="tab"]');
+    $('a.navbar-brand').on('click', function (event) {
+        event.preventDefault();
+        $tabs.first().trigger('click');
+    });
+    $tabs.on('click', function (event) {
+        var tab = $(event.target).attr('href').replace('#', '');
+        if (tab !== window.location.hash.replace('#', '')) {
+            window.location.hash = tab;
+        }
+        loadFrame($('#' + tab).find('iframe'));
+    });
+    $(window).on('hashchange', function () {
+        var hash = window.location.hash.replace('#', '');
+        if ('' === hash) {
+            $tabs.first().trigger('click');
+        } else {
+            $tabs.filter('[href="#' + hash + '"]').trigger('click');
+        }
+    }).trigger('hashchange');
+});
---
+++ gui/public/pages/adminer.php
@@ -0,0 +1,14 @@
+<?php
+
+if (!is_dir('../../vendor/vrana/adminer')) {
+    chdir('../../fallback');
+} else {
+    chdir('../../vendor/vrana/adminer');
+}
+
+$db_adminer_file = array_values(array_filter(scandir('.', SCANDIR_SORT_DESCENDING), function ($file) {
+    $name = 'adminer';
+    return $name === substr($file, 0, strlen($name));
+}))[0];
+
+include $db_adminer_file;
